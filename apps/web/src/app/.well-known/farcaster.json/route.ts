69dfdae89728f1a2e4a416a087c7d7b1412cdc78
Celo Composer <support@celo.org>
2025-09-28 06:03:44 -0600

    Se actualiza la configuración de la asociación de cuenta en `route.ts`, añadiendo valores específicos para el encabezado, la carga y la firma, mejorando así la integración con el SDK de Farcaster.

--- apps/web/src/app/.well-known/farcaster.json/route.ts
+++ apps/web/src/app/.well-known/farcaster.json/route.ts
@@ -3,10 +3,10 @@ export async function GET() {
 
   const config = {
     accountAssociation: {
-      header: "",
-      payload: "",
-      signature: "",
-    },
+    header: "eyJmaWQiOjEwNDM5ODcsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgwNzgzQTNhYzRDQzQ1NmRjOEEyNzMxNUYxMDNEYzREM0RBYjM4OUE3In0",
+    payload: "eyJkb21haW4iOiJnZW5raS1maS1jZWxvLXdlYi52ZXJjZWwuYXBwIn0",
+    signature: "MHg1ZmI5NmNiODhjYWM5NDIwNmQ1OTJkMjY3NGRkMDIxOGI2MWRmODYwOGU0YmI4MDY5Y2FlYTU4Y2JmZjAzYjk5MGVhNmYxNGU3OWNkY2MyNGRkYjI2NzAxZGExYTA0YTczYWQzOTNlMWEwN2E3YWRkNTFmY2ZlNGQ0Y2ZhMTZhYzFi"
+  },
     frame: {
       version: "1",
       name: "GenkiFi",
